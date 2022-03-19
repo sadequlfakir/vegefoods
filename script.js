e83cf491ca2671af9c14b4da782542fbd1a4fab6
sadequlfakir <sadikul0140@gmail.com>
2022-03-19 20:21:30 +0600

    full site add

---
+++ script.js
@@ -0,0 +1,9 @@
+function sidebarHandler(e) {
+    const sideBar = document.getElementById('sidebar').classList.toggle('active');
+};
+function msubmenuHandler(e) {
+    const sideBar = document.getElementById('sub-menu').classList.toggle('active');
+};
+function submenuHandler(e) {
+    const sideBar = document.getElementById('submenu').classList.toggle('active');
+};
